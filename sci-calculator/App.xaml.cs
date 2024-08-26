fab1c8225e74e4349360c0d9bc88964ec3717226
Opilane TTHK <andero.magi.420+schoolgit@gmail.com>
2024-08-26 13:13:48 +0300

    Initial commit

---
+++ sci-calculator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace sci_calculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ sci-calculator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace sci_calculator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
