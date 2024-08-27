6f13ed79671dfef496aae5f9d8afc6d174b7416e
Opilane TTHK <andero.magi.420+schoolgit@gmail.com>
2024-08-27 14:07:35 +0300

    This still isn't working

--- sci-calculator/App.xaml.cs
+++ sci-calculator/App.xaml.cs
@@ -1,10 +1,33 @@
-﻿namespace sci_calculator
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+namespace sci_calculator
 {
     public partial class App : Application
     {
+        const int WindowHeight = 540;
+        const int WindowWidth = 1000;
+
         public App()
         {
-            InitializeComponent();
+            InitializeComponent(); Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            {
+#if WINDOWS
+                var mauiWindow = handler.VirtualView;
+                var nativeWindow = handler.PlatformView;
+
+                nativeWindow.Activate();
+
+                IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+                AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+
+                appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+#endif
+            });
 
             MainPage = new AppShell();
         }
