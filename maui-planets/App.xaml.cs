0ebe7537cf3beb3884ad5b076e5538e8ff646581
Opilane TTHK <andero.magi.420+schoolgit@gmail.com>
2024-09-09 13:17:56 +0300

    create maui planets app

---
+++ maui-planets/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using maui_planets.Views;
+
+namespace maui_planets
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new StartPage();
+        }
+    }
+}
---
+++ maui-planets/Views/PlanetsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace maui_planets.Views;
+
+public partial class PlanetsPage : ContentPage
+{
+	public PlanetsPage()
+	{
+		InitializeComponent();
+	}
+}
