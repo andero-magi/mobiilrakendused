44615dffdbe7f3fc1a947fdb75447290d01c53c0
Opilane TTHK <andero.magi.420+schoolgit@gmail.com>
2024-09-10 15:03:01 +0300

    more stuff

--- maui-planets/Views/PlanetsPage.xaml.cs
+++ maui-planets/Views/PlanetsPage.xaml.cs
@@ -1,9 +1,39 @@
+using maui_planets.Models;
+using Microsoft.Maui.Controls;
+
 namespace maui_planets.Views;
 
 public partial class PlanetsPage : ContentPage
 {
+	public uint AnimationDuration = 20;
+
 	public PlanetsPage()
 	{
 		InitializeComponent();
+	}
+
+	async void ProfilePic_Tapped(System.Object sender, System.EventArgs e)
+	{
+		_ = MainContentGrid.TranslateTo(-this.Width * 0.5, this.Height * 0.1, AnimationDuration, Easing.CubicIn);
+		await MainContentGrid.ScaleTo(0.8, AnimationDuration);
+		_ = MainContentGrid.ScaleTo(0.8, AnimationDuration);
+	}
+
+	async void Planets_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
+	{
+		await Navigation.PushAsync(new PlanetsDetailPage(e.CurrentSelection.First() as Planet));
+	}
+
+	async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+	{
+		await CloseMenu();
+	}
+
+	private async Task CloseMenu()
+	{
+		_ = MainContentGrid.FadeTo(1, AnimationDuration);
+		_ = MainContentGrid.ScaleTo(1, AnimationDuration);
+
+		await MainContentGrid.TranslateTo(0, 0, AnimationDuration, Easing.CubicIn);
 	}
 }
